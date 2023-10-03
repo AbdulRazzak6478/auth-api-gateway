1cffe881fd3e022c5601c71e93b5bb751a8cddda
Abdul Razzak <abdulrazzak74832@gmail.com>
2023-10-04 02:05:57 +0530

    Added rate limiters into the project

--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@ const express = require('express');
 const {ServerConfig:{PORT},Logger} = require('./config');
 const apiRoutes =require('./routes');
 const { Auth } = require('./utils/common');
+const rateLimit = require('express-rate-limit');
 
 const app = express();
 
+const limiter = rateLimit({
+    windowMs: 2 * 60 *1000, // 2 minutes
+    max: 3, // limit each IP to 2 requests per 'window' (here , per 2 minutes)
+})
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+app.use(limiter);
 
 app.use('/api',apiRoutes);
 
@@ -14,7 +22,4 @@ app.use('/api',apiRoutes);
 app.listen(PORT,()=>{
     console.log(`Successfully started the server on PORT ${PORT} `); 
     Logger.info("Successfully started server"," root ", {});
-    // const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZW1haWwiOiJhQGIuY29tIiwiaWF0IjoxNjk2Mjg1MTgwLCJleHAiOjE2OTYyODg3ODB9.v2DV3-9IsM3JZsP-UUCPvu-kAKWtRlzLOc8_LjqBz_o';
-    // const response = Auth.verifyToken(token);
-    // console.log(response);
 });
